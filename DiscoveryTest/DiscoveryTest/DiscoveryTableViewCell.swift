acd1aa6c398e6a09e1df329511bb2ebf5d7be6df
Jason <jasonchitla@hotmail.com>
2016-05-17 13:41:27 -0400

    jchitla/secondCommit

---
+++ DiscoveryTest/DiscoveryTest/DiscoveryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DiscoveryTableViewCell.swift
+//  DiscoveryTest
+//
+//  Created by Jason Chitla on 5/17/16.
+//  Copyright © 2016 Jason Chitla. All rights reserved.
+//
+
+import UIKit
+
+class DiscoveryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
