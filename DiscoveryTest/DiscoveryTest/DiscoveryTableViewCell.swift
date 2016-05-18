0690a581e06f84c98a36dfec23dcedaf89a656f9
Jason <jasonchitla@hotmail.com>
2016-05-18 12:04:53 -0400

    jchitla/ThirdFixCommit

--- DiscoveryTest/DiscoveryTest/DiscoveryTableViewCell.swift
+++ DiscoveryTest/DiscoveryTest/DiscoveryTableViewCell.swift
@@ -8,18 +8,17 @@
 
 import UIKit
 
-final class DiscoveryTableViewCell: UITableViewCell {
+final class DiscoveryTableViewCell: UITableViewCell, TableViewCellReusable {
     
     // Mark: Properties
-    
     @IBOutlet private weak var nameLabel: UILabel!
 
-    /*override func awakeFromNib() {
+    override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
+    /*override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
--- DiscoveryTest/DiscoveryTest/DiscoveryTableViewController.swift
+++ DiscoveryTest/DiscoveryTest/DiscoveryTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class DiscoveryTableViewController: UITableViewController, UISearchBarDelegate {
+final class DiscoveryTableViewController: UITableViewController, UISearchBarDelegate, AccessTicketMasterAPI {
     
     // Mark: Properties
     
@@ -20,7 +20,7 @@ final class DiscoveryTableViewController: UITableViewController, UISearchBarDele
         
         searchBar.delegate = self
         
-        callJSON()
+        getRecentEventsIntoConsole()
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,68 +40,14 @@ final class DiscoveryTableViewController: UITableViewController, UISearchBarDele
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
-        // Cell identifier
-        
-        let cellIdentifier = "DiscoveryTableViewCell"
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! DiscoveryTableViewCell
+        let dequeuedCell = tableView.dequeueReusableCellWithIdentifier(DiscoveryTableViewCell.reIdentifier, forIndexPath: indexPath) as! DiscoveryTableViewCell
         
         // Return appropriate event
         
         let event = events[indexPath.row]
-        cell.configureWithEvent(event)
+        dequeuedCell.configureWithEvent(event)
 
-        return cell
-    }
-    
-    // Mark: Handling API Request
-    
-    func callJSON() {
-        let endpoint: String = "https://app.ticketmaster.com/discovery/v1/events.json?apikey=WaPwayOHGN4PCY1EieuT2nCM5H8tufYf"
-        guard let url = NSURL(string: endpoint) else {
-            print("Error: cannot create URL")
-            return
-        }
-        let urlRequest = NSURLRequest(URL: url)
-        
-        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
-        let session = NSURLSession(configuration: config)
-        
-        let task = session.dataTaskWithRequest(urlRequest) {
-            (data, response, error) in
-            // check for any errors
-            guard error == nil else {
-                print("error calling GET on /discovery/v1")
-                print(error)
-                return
-            }
-            // make sure there's data
-            guard let responseData = data else {
-                print("Error: did not receive data")
-                return
-            }
-            // parse result as JSON
-            do {
-                guard let object = try NSJSONSerialization.JSONObjectWithData(responseData, options: []) as? [String: AnyObject] else {
-                    print("error trying to convert data to JSON")
-                    return
-                }
-                // print object
-                //print("Here is the object: " + object.description)
-                
-                // access event names
-                let eventArray = object["_embedded"]!["events"] as! [[String: AnyObject]]
-                
-                for event in eventArray {
-                    print("*********************************************************************************************")
-                    print(event["name"]!)
-                }
-                
-            } catch  {
-                print("error trying to convert data to JSON")
-                return
-            }
-        }
-        task.resume()
+        return dequeuedCell
     }
 
 }
